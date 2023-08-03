80cb9e71fd8ef23b9083b83ae436a00aee1aac3e
Roman <rpilyushin@gmail.com>
2023-08-03 14:12:35 +0300

    Add Ch 4 Source Code

---
+++ chapter04/main3.cpp
@@ -0,0 +1,58 @@
+#include <algorithm>
+#include <iomanip>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+#include "grade.h"
+#include "Student_info.h"
+
+using std::cin;                     using std::setprecision;
+using std::cout;                    using std::sort;
+using std::domain_error;            // using std::streamsize;
+using std::endl;                    using std::string;
+using std::max;                     using std::vector;
+using std::begin;					using std::end;
+
+/*
+int main()
+{
+	vector<Student_info> students;
+	Student_info record;
+	string::size_type maxlen = 0;       // the length of the longest name
+
+	// read and store all the students' data.
+	// Invariant:	`students' contains all the student records read so far
+	//			`maxlen' contains the length of the longest name in `students'
+	while (read(cin, record)) {
+		// find length of longest name
+		maxlen = max(maxlen, record.name.size());
+		students.push_back(record);
+	}
+
+	// alphabetize the student records
+	sort(begin(students), end(students), compare);
+
+	// write the names and grades
+	for (auto student : students) {
+		// write the name, padded on the right to `maxlen' `+' `1' characters
+		cout << student.name
+			<< string(maxlen + 1 - student.name.size(), ' ');
+
+		// compute and write the grade
+		try {
+			double final_grade = grade(student);
+			// streamsize 
+			auto prec = cout.precision();
+			cout << setprecision(3) << final_grade
+				<< setprecision(prec);
+		}
+		catch (const std::domain_error& e) {
+			cout << e.what();
+		}
+		cout << endl;
+	}
+	return 0;
+}
+*/
---
+++ chapter04/median.cpp
@@ -0,0 +1,29 @@
+// source file for the `median' function
+#include <algorithm>    // to get the declaration of `sort'
+#include <stdexcept>    // to get the declaration of `domain_error'
+#include <vector>       // to get the declaration of `vector'
+
+using std::domain_error;   	using std::sort;   using std::vector;
+using std::begin;			using std::end;
+
+#include "median.h"
+
+// compute the median of a `vector<double>'
+// note that calling this function copies the entire argument `vector'
+double median(vector<double> vec)
+{
+	// typedef vector<double>::size_type vec_sz;
+	// using vec_sz = vector<double>::size_type;
+
+	// vec_sz 
+	auto size = vec.size();
+	if (size == 0)
+		throw domain_error("median of an empty vector");
+
+	sort(begin(vec), end(vec));
+
+	// vec_sz 
+	auto mid = size / 2;
+
+	return size % 2 == 0 ? (vec[mid] + vec[mid - 1]) / 2 : vec[mid];
+}
